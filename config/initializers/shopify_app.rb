c063dd8b84ac9deb427c62f03821afc5fcfb8ad6
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-07 22:48:21 +0100

    updated redirect uri

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = 'Fera.ai Loyalty Program'
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
+  config.redirect_uri = ENV['REDIRECT_URI']
   config.old_secret = ''
   config.scope = 'read_products' # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
