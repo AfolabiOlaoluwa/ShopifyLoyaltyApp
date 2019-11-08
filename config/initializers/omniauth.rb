8af8b9a9d12720c9ac38027d017fbf2b7e08412e
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-08 23:58:18 +0100

    updated app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            ShopifyApp.configuration.api_key,
            ShopifyApp.configuration.secret,
            scope: ShopifyApp.configuration.scope,
-           callback_path: '/auth/shopify/callback'
+           callback_path: 'auth/shopify/callback'
 end
