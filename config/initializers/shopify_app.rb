12592813ac3bcfbe44c2e3a7dcd2099d0b7a91ea
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-08 17:03:20 +0100

    updated app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "Fera Loyalty Program"
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
-  config.old_secret = ""
   config.scope = "read_orders, write_orders, read_products" # read_all_orders
   config.embedded_app = true
   # config.after_authenticate_job = false
