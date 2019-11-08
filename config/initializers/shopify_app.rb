422675d58c875929ce2b9bbb6f7335c71a4cf6d3
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-08 16:40:36 +0100

    updated app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ''
-  config.scope = 'read_orders, read_products'
+  config.scope = 'read_orders, write_orders, read_products' #read_all_orders
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = '2019-10'
