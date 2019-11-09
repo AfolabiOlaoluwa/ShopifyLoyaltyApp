fb9d94525285b84e2ff1eaed6d373277fb895d0c
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-09 23:35:15 +0100

    upgraded shopify gem

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,14 +1,13 @@
+# frozen_string_literal: true
+
 ShopifyApp.configure do |config|
   config.application_name = "Fera Loyaly"
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = ""
-  config.scope = "read_products"
+  config.scope = "read_orders, write_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.api_version = "2019-10"
   config.session_repository = Shop
 end
-
-# ShopifyApp::Utils.fetch_known_api_versions                        # Uncomment to fetch known api versions from shopify servers on boot
-# ShopifyAPI::ApiVersion.version_lookup_mode = :raise_on_unknown    # Uncomment to raise an error if attempting to use an api version that was not previously known
