afcd2718c16d0d52d1f9578658e660361e8df441
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-12 00:20:37 +0100

    update

--- app/models/earning_rule.rb
+++ app/models/earning_rule.rb
@@ -1,3 +1,7 @@
 class EarningRule < ApplicationRecord
   has_many :customer_details
+
+  validates :name, presence: true
+  validates :point, presence: true
+  validates :status, presence: true
 end
