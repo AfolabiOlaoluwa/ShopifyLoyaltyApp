f17b7da9187f05e53d13961803e518920d87ec1a
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-12 16:37:15 +0100

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
     presenter = klass.new(model, self)
     yield(presenter) if block_given?
   end
+
+  def text_formatter(value, true_color = 'success', false_color = 'danger')
+    case value
+    when true
+      content_tag(:span, 'Active', class: "btn-sm tag-#{true_color}")
+    when false
+      content_tag(:span, 'Inactive', class: "btn-sm tag-#{false_color}")
+    end
+  end
 end
--- app/models/earning_rule.rb
+++ app/models/earning_rule.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class EarningRule < ApplicationRecord
   has_many :customer_details
 
   validates :name, presence: true
   validates :point, presence: true
-  # validates :status, presence: true
 end
