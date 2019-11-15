e0c0e69a9760700671671ffb8bc2fc06bcd0c904
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-15 18:24:04 +0100

    make it better

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['PROD_REDIS_URL'] }
+  config.redis = { url: ENV['PROD_REDIS_URL'], size: 9 }
   # config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['PROD_REDIS_URL'] }
+  config.redis = { url: ENV['PROD_REDIS_URL'], size: 1 }
   # config.redis = { url: ENV['REDIS_PROVIDER'] }
 end
