90351d11350d27d54a86c25929cbf039a1aa3e04
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2019-11-21 16:40:14 +0100

    make it better

--- app/controllers/customer_details_controller.rb
+++ app/controllers/customer_details_controller.rb
@@ -4,6 +4,6 @@ class CustomerDetailsController < AuthenticatedController
   before_action :authenticate_user!
 
   def index
-    @customer_details = customer_details.shop.load
+    @customer_details = customer_detail.shop.load
   end
 end
